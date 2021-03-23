c3fe86b016c998f93a946517afb04ec617d50bbc
Vadim Korolev <vkorolev1506@gmail.com>
2021-03-23 14:40:10 +0300

    upload assets

---
+++ app/assets/icons/LWicon.tsx
@@ -0,0 +1,11 @@
+import {SvgIcon} from '@material-ui/core'
+import React from 'react'
+
+export default function LWicon(props:any) {
+    return (
+        <SvgIcon {...props}>
+            <path  d="M3.3,5.5h2.5l1.9,10h1.9l0.4,2H5.6L3.3,5.5z"/>
+            <path  d="M11.1,5.5l1.3,9h0l1.4-9h2.4l1.4,9h0l1.3-9h2.4l-2.2,12h-2.8l-1.4-8h0l-1.4,8h-2.8l-2.2-12H11.1z"/>
+        </SvgIcon>
+    )
+}
