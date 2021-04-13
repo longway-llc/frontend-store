23bb47b1afac2a97275268f33cf39f78a40b4fda
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 20:57:34 +0300

    dev: create useTranslation hook

---
+++ app/utils/localization.ts
@@ -0,0 +1,12 @@
+import {useCallback} from 'react'
+import {en} from '../locales/en'
+import {ru} from '../locales/ru'
+
+
+export type CurrentLocale = 'ru' | 'en' | undefined
+
+
+export function useTranslation(locale: string | undefined) {
+    const t = useCallback(() => locale == 'ru' ? ru : en, [locale])
+    return t()
+}
