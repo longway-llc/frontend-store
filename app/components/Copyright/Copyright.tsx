571cb3c2e33e6d71fe0d945b9036d79bc6695fba
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 21:39:37 +0300

    Create Copyright.tsx

---
+++ app/components/Copyright/Copyright.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react'
+import {Typography} from '@material-ui/core'
+import Link from 'next/link'
+
+const Copyright:FC = () => {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link href={process.env.NEXTAUTH_URL as string}>
+                lwaero
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
+
+export default Copyright
