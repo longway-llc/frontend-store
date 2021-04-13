4c9a8b217116c1f5171f3944578ad023049faa57
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 21:39:48 +0300

    Create HeaderSearchInput.tsx

---
+++ app/components/HeaderSearchInput/HeaderSearchInput.tsx
@@ -0,0 +1,80 @@
+import React, {FC} from 'react'
+import {createStyles, FormControl, Input, InputAdornment, InputLabel, makeStyles, Theme} from '@material-ui/core'
+import {Search} from '@material-ui/icons'
+
+import {useSearch} from '../../utils/useSearch'
+import {HeaderMode, HeaderModeType} from '../Header/Header'
+import {useRouter} from 'next/router'
+import {useTranslation} from '../../utils/localization'
+
+type StyleProps = {
+    headerMode: HeaderMode
+}
+
+const useStyles = makeStyles<Theme, StyleProps>(theme => createStyles({
+    root: {
+        '& > .MuiInputLabel-root.Mui-focused': {
+            color: props => (props.headerMode == 'main')
+                ? theme.palette.common.white
+                : theme.palette.common.black
+        },
+        '& > .MuiInput-underline:before': {
+            borderColor: props => (props.headerMode == 'main')
+                ? theme.palette.common.white
+                : theme.palette.common.black,
+        },
+    },
+    label: {
+        color: props => (props.headerMode == 'main')
+            ? theme.palette.common.white
+            : theme.palette.common.black
+    },
+    input: {
+        color: props => (props.headerMode == 'main')
+            ? theme.palette.common.white
+            : theme.palette.common.black,
+        '&:focus': {
+            color: props => (props.headerMode == 'main')
+                ? theme.palette.common.white
+                : theme.palette.common.black
+        }
+    },
+    icon: {
+        color: props => (props.headerMode == 'main')
+            ? theme.palette.common.white
+            : theme.palette.common.black
+    }
+}))
+
+const HeaderSearchInput: FC<HeaderModeType> = ({headerMode = 'main'}) => {
+    const styles = useStyles({headerMode})
+    const {searchRequest, searchItem, setSearchRequest} = useSearch()
+
+    const locale = useRouter()?.locale
+    const t = useTranslation(locale)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearchRequest(e.target.value)
+    return (
+        <FormControl
+            className={styles.root}
+            onSubmit={searchItem}
+        >
+            <InputLabel className={styles.label} htmlFor='search-header'>
+                {t.components.SearchField.shortLabel}
+            </InputLabel>
+            <Input
+                className={styles.input}
+                value={searchRequest}
+                id="search-header"
+                startAdornment={
+                    <InputAdornment position="start" className={styles.icon}>
+                        <Search/>
+                    </InputAdornment>
+                }
+                onChange={handleChange}
+            />
+        </FormControl>
+    )
+}
+
+export default HeaderSearchInput 
