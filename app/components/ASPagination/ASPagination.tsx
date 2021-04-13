6386503404f6e1550fe05e3d03538cb074ba55f0
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 21:38:43 +0300

    Create ASPagination.tsx

---
+++ app/components/ASPagination/ASPagination.tsx
@@ -0,0 +1,34 @@
+import React, {FC} from 'react'
+import {connectPagination} from 'react-instantsearch-dom'
+import {Pagination} from '@material-ui/lab'
+import {createStyles, makeStyles} from '@material-ui/core'
+
+
+const useStyles = makeStyles(theme => createStyles({
+    root: {
+        marginBottom: theme.spacing(4)
+    }
+}))
+
+interface PaginationProps {
+    currentRefinement: number,
+    nbPages: number
+    refine: (args: any) => any
+}
+
+
+const ASPagination: FC<PaginationProps> = ({currentRefinement, nbPages, refine}) => {
+    const styles = useStyles()
+    const [page, setPage] = React.useState(currentRefinement)
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value)
+        refine(value)
+    }
+
+    return (
+        <Pagination className={styles.root} hidePrevButton hideNextButton count={nbPages} page={page}
+                    onChange={handleChange}/>
+    )
+}
+
+export default connectPagination(ASPagination)
