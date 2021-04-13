199d9cc63b79f1a89598072e710a6bdff0c43466
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 21:05:01 +0300

    fix: include directory

--- app/apollo.config.js
+++ app/apollo.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: {
         excludes: ['**/.next/**/*', '**/public/**/*', '**/assets/**/*', '**/__tests__/**/*', '**/node_modules/**/*'],
+        includes: ['**/*.ts*'],
         service: {
             name: 'strapi-cms',
             url: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
             localSchemaFile: './.graphql/schema.graphql',
             output: './__generated__',
-            includes: ['**/*.tsx'],
         }
     }
 }
