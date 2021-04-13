49fc0ef0fa8c43c9bec8ef7aee36bb19813d16f1
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 21:43:58 +0300

    Create globalTypes.ts

---
+++ app/__generated__/globalTypes.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum ENUM_ORDER_STATUS {
+  closed = "closed",
+  inProgress = "inProgress",
+  open = "open",
+  paid = "paid",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
