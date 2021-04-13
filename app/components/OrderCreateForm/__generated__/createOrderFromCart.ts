deaaf09caa7c436598f1a91aefc8096ba7ea74aa
Vadim Korolev <vkorolev1506@gmail.com>
2021-04-13 21:39:55 +0300

    Create createOrderFromCart.ts

---
+++ app/components/OrderCreateForm/__generated__/createOrderFromCart.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: createOrderFromCart
+// ====================================================
+
+export interface createOrderFromCart_createOrderFromCart {
+  __typename: "Order";
+  id: string;
+}
+
+export interface createOrderFromCart {
+  createOrderFromCart: createOrderFromCart_createOrderFromCart;
+}
+
+export interface createOrderFromCartVariables {
+  locale: string;
+}
