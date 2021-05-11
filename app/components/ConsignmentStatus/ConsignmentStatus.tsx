b4cc6ae23eed5a6516db24daf70fc13924df243e
Vadim Korolev <vkorolev1506@gmail.com>
2021-05-11 17:26:31 +0300

    dev: add excepted date display

--- app/components/ConsignmentStatus/ConsignmentStatus.tsx
+++ app/components/ConsignmentStatus/ConsignmentStatus.tsx
@@ -1,15 +1,17 @@
 import React, {FC} from 'react'
 import {useSession} from 'next-auth/client'
 import {Box, createStyles, Grid, makeStyles, Typography} from '@material-ui/core'
-import {green, orange} from '@material-ui/core/colors'
+import {green, grey, orange} from '@material-ui/core/colors'
 import {theme} from '../../assets/theme'
 import ConsignmentDisplay from '../ConsignmentDisplay/ConsignmentDisplay'
 import {useRouter} from 'next/router'
 import {useTranslation} from '../../utils/localization'
+import moment from 'moment'
 
 
 type ConsignmentStatusProps = {
-    consignments: Array<any>
+    consignments: Array<any>,
+    expectedDeliveryDate: string
 }
 
 
@@ -28,17 +30,22 @@ const useStyles = makeStyles(() => createStyles({
     },
     mt1: {
         marginTop: theme.spacing(1)
+    },
+    ml1: {
+        marginLeft: theme.spacing(1),
+        color: grey['500']
     }
 }))
 
-const ConsignmentStatus: FC<ConsignmentStatusProps> = ({consignments}) => {
+const ConsignmentStatus: FC<ConsignmentStatusProps> = ({consignments, expectedDeliveryDate}) => {
     const styles = useStyles()
     const {locale} = useRouter()
     const t = useTranslation(locale)
-    const [session, loading] = useSession()
+    const [session] = useSession()
 
     if (!session?.user) {
-        return <Typography variant={'subtitle1'} className={styles.available}>{t.components.ConsignmentStatus.available}!</Typography>
+        return <Typography variant={'subtitle1'}
+                           className={styles.available}>{t.components.ConsignmentStatus.available}!</Typography>
     }
 
     return (
@@ -54,9 +61,16 @@ const ConsignmentStatus: FC<ConsignmentStatusProps> = ({consignments}) => {
                         </Box>
                     </>
                     :
-                    <Typography variant={'subtitle1'} className={styles.notAvailable}>
-                        {t.components.ConsignmentStatus.unavailable}!
-                    </Typography>
+                    <>
+                        {
+                            expectedDeliveryDate && <Typography variant={'overline'} className={styles.ml1}>
+                                {t.components.ConsignmentStatus.expectedDelivery}: <b>{moment(expectedDeliveryDate).locale(locale ?? 'en').format('L')}</b>
+                            </Typography>
+                        }
+                        <Typography variant={'subtitle1'} className={styles.notAvailable}>
+                            {t.components.ConsignmentStatus.unavailable}!
+                        </Typography>
+                    </>
                 }
             </Grid>
         </Grid>
