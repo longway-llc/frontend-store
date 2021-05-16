e19d604bc7b30b18422c77f11cba9441998dd7e9
Vadim Korolev <49880037+VadimOnix@users.noreply.github.com>
2021-05-16 12:00:33 +0300

    dev: add available product status in search hits

--- app/components/ASHitConsignmentStatus/ASHitConsignmentStatus.tsx
+++ app/components/ASHitConsignmentStatus/ASHitConsignmentStatus.tsx
@@ -1,51 +1,53 @@
 import React from 'react'
-import {createStyles, makeStyles, Typography} from '@material-ui/core'
-import {useRouter} from 'next/router'
-import {useTranslation} from '../../utils/localization'
-import {green, orange} from '@material-ui/core/colors'
+import { createStyles, makeStyles, Typography } from '@material-ui/core'
+import { useRouter } from 'next/router'
+import { useTranslation } from '../../utils/localization'
+import { green, orange } from '@material-ui/core/colors'
+import { useSession } from 'next-auth/client'
 
-
-const useStyles = makeStyles(theme => createStyles({
-    root:{
-        marginRight: theme.spacing(1),
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginRight: theme.spacing(1),
     },
     available: {
-        padding: theme.spacing(0,1),
-        color: theme.palette.common.white,
-        textAlign: 'center',
-        backgroundColor: green['200'],
-        borderRadius: `${theme.shape.borderRadius}px`
+      padding: theme.spacing(0, 1),
+      color: theme.palette.common.white,
+      textAlign: 'center',
+      backgroundColor: green['200'],
+      borderRadius: `${theme.shape.borderRadius}px`,
     },
     notAvailable: {
-        padding: theme.spacing(0,1),
-        color: theme.palette.common.white,
-        textAlign: 'center',
-        backgroundColor: orange['200'],
-        borderRadius: `${theme.shape.borderRadius}px`
+      padding: theme.spacing(0, 1),
+      color: theme.palette.common.white,
+      textAlign: 'center',
+      backgroundColor: orange['200'],
+      borderRadius: `${theme.shape.borderRadius}px`,
     },
-}))
+  })
+)
 
-const AsHitConsignmentStatus = ({consignmnets}:any) => {
-    const styles = useStyles()
+const AsHitConsignmentStatus = ({ consignmnets }: any) => {
+  const styles = useStyles()
+  const [session] = useSession()
+  const locale = useRouter()?.locale
+  const t = useTranslation(locale)
 
-    const locale = useRouter()?.locale
-    const t = useTranslation(locale)
+  const available = consignmnets?.length > 0
 
-    const available = consignmnets?.length > 0
-
-    return (
-        <div className={styles.root}>
-            {available ?
-                <Typography variant={'subtitle1'} className={styles.available}>
-                    {t.components.AsHitConsignmentStatus.available}
-                </Typography>
-                :
-                <Typography variant={'subtitle1'} className={styles.notAvailable}>
-                    {t.components.AsHitConsignmentStatus.unavailable}
-                </Typography>
-            }
-        </div>
-    )
+  return (
+    <div className={styles.root}>
+      {available || !session?.user ? (
+        <Typography variant={'subtitle1'} className={styles.available}>
+          {t.components.AsHitConsignmentStatus.available}
+        </Typography>
+      ) : (
+        <Typography variant={'subtitle1'} className={styles.notAvailable}>
+          {t.components.AsHitConsignmentStatus.unavailable}
+        </Typography>
+      )}
+    </div>
+  )
 }
 
 export default AsHitConsignmentStatus
--- app/components/ConsignmentStatus/ConsignmentStatus.tsx
+++ app/components/ConsignmentStatus/ConsignmentStatus.tsx
@@ -1,80 +1,77 @@
-import React, {FC} from 'react'
-import {useSession} from 'next-auth/client'
-import {Box, createStyles, Grid, makeStyles, Typography} from '@material-ui/core'
-import {green, grey, orange} from '@material-ui/core/colors'
-import {theme} from '../../assets/theme'
+import React, { FC } from 'react'
+import { useSession } from 'next-auth/client'
+import { Box, createStyles, Grid, makeStyles, Typography } from '@material-ui/core'
+import { green, grey, orange } from '@material-ui/core/colors'
+import { theme } from '../../assets/theme'
 import ConsignmentDisplay from '../ConsignmentDisplay/ConsignmentDisplay'
-import {useRouter} from 'next/router'
-import {useTranslation} from '../../utils/localization'
-import moment from 'moment'
-
+import { useRouter } from 'next/router'
+import { useTranslation } from '../../utils/localization'
 
 type ConsignmentStatusProps = {
-    consignments: Array<any>,
-    expectedDeliveryDate: string
+  consignments: Array<any>
+  expectedDeliveryDate: string
 }
 
-
-const useStyles = makeStyles(() => createStyles({
+const useStyles = makeStyles(() =>
+  createStyles({
     available: {
-        color: green['700'],
-        textAlign: 'center',
-        border: `1px solid ${green['200']}`,
-        borderRadius: `${theme.shape.borderRadius}px`
+      color: green['700'],
+      textAlign: 'center',
+      border: `1px solid ${green['200']}`,
+      borderRadius: `${theme.shape.borderRadius}px`,
     },
     notAvailable: {
-        color: orange['700'],
-        textAlign: 'center',
-        border: `1px solid ${orange['200']}`,
-        borderRadius: `${theme.shape.borderRadius}px`
+      color: orange['700'],
+      textAlign: 'center',
+      border: `1px solid ${orange['200']}`,
+      borderRadius: `${theme.shape.borderRadius}px`,
     },
     mt1: {
-        marginTop: theme.spacing(1)
+      marginTop: theme.spacing(1),
     },
     ml1: {
-        marginLeft: theme.spacing(1),
-        color: grey['500']
-    }
-}))
+      marginLeft: theme.spacing(1),
+      color: grey['500'],
+    },
+  })
+)
 
-const ConsignmentStatus: FC<ConsignmentStatusProps> = ({consignments, expectedDeliveryDate}) => {
-    const styles = useStyles()
-    const {locale} = useRouter()
-    const t = useTranslation(locale)
-    const [session] = useSession()
+const ConsignmentStatus: FC<ConsignmentStatusProps> = ({ consignments, expectedDeliveryDate }) => {
+  const styles = useStyles()
+  const { locale } = useRouter()
+  const t = useTranslation(locale)
+  const [session] = useSession()
 
-    if (!session?.user) {
-        return <Typography variant={'subtitle1'}
-                           className={styles.available}>{t.components.ConsignmentStatus.available}!</Typography>
-    }
-
+  if (!session?.user) {
     return (
-        <Grid container>
-            <Grid item xs={12}>
-                {consignments.length > 0 ?
-                    <>
-                        <Typography variant={'subtitle1'} className={styles.available}>
-                            {t.components.ConsignmentStatus.available}!
-                        </Typography>
-                        <Box className={styles.mt1}>
-                            <ConsignmentDisplay consignments={consignments}/>
-                        </Box>
-                    </>
-                    :
-                    <>
-                        {
-                            expectedDeliveryDate && <Typography variant={'overline'} className={styles.ml1}>
-                                {t.components.ConsignmentStatus.expectedDelivery}: <b>{moment(expectedDeliveryDate).locale(locale ?? 'en').format('L')}</b>
-                            </Typography>
-                        }
-                        <Typography variant={'subtitle1'} className={styles.notAvailable}>
-                            {t.components.ConsignmentStatus.unavailable}!
-                        </Typography>
-                    </>
-                }
-            </Grid>
-        </Grid>
+      <Typography variant={'subtitle1'} className={styles.available}>
+        {t.components.ConsignmentStatus.available}!
+      </Typography>
     )
+  }
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        {consignments.length > 0 ? (
+          <>
+            <Typography variant={'subtitle1'} className={styles.available}>
+              {t.components.ConsignmentStatus.available}!
+            </Typography>
+            <Box className={styles.mt1}>
+              <ConsignmentDisplay consignments={consignments} />
+            </Box>
+          </>
+        ) : (
+          <>
+            <Typography variant={'subtitle1'} className={styles.notAvailable}>
+              {t.components.ConsignmentStatus.unavailable}!
+            </Typography>
+          </>
+        )}
+      </Grid>
+    </Grid>
+  )
 }
 
 export default ConsignmentStatus
